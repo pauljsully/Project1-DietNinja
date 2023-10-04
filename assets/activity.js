76aa3b218b3d4b9b4b36bac58850032f3b9838f8
Paul Sullivan <paul8sullivan@gmail.com>
2023-10-04 01:24:51 -0400

    updated activity.js to add up calories

--- assets/activity.js
+++ assets/activity.js
@@ -1,27 +1,58 @@
-// Function to handle adding activities
 document.addEventListener('DOMContentLoaded', function() {
     var activityForm = document.querySelector('.activity-content form');
+    var calorieTotalElement = document.getElementById('calorie-total');
+    var totalCalories = 0; // Initialize total calories
+
     activityForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent form submission
 
         var activityInput = document.getElementById('activity-item');
         var activity = activityInput.value;
+        var activityList = document.querySelector('ul');
 
+        var currentTime = dayjs().format('h:mm a');
     
-        $.ajax({
+
+        // Display the current time next to the activity input
+        
+
+        var url = 'https://api.api-ninjas.com/v1/caloriesburned?activity=' + activity;
+        var apiKey = 'tVmjVkv8ropSYzdiyJMI8A==4xxqPwtUpB4dDOmg';
+
+        fetch(url, {
             method: 'GET',
-            url: 'https://api.api-ninjas.com/v1/caloriesburned?activity=' + activity,
-            headers: { 'X-Api-Key': 'tVmjVkv8ropSYzdiyJMI8A==4xxqPwtUpB4dDOmg'},
-            contentType: 'application/json',
-            success: function(result) {
-                console.log(result);
+            headers: {
+                'X-Api-Key': apiKey,
             },
-            error: function ajaxError(jqXHR) {
-                console.error('Error: ', jqXHR.responseText);
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Error response');
             }
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result);
+
+            for (var i = 0; i < 1; i++) {
+                var listActivity = document.createElement('li');
+                // Concatenate activity name and total calories with a separator
+                var activityCalories = result[i].total_calories;
+                listActivity.textContent = activity + ' - ' + activityCalories + ' cal' + " (" + currentTime + ")";
+                activityList.appendChild(listActivity);
+
+                // Update total calories
+                totalCalories += activityCalories;
+                calorieTotalElement.textContent = 'Activity calories: ' + totalCalories;
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error.message);
         });
 
         // Clear the input field after adding the activity
         activityInput.value = '';
     });
+
+    
 });
